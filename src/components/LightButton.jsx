c6c5f2f6ad3a8bd52522a6d9678213d0680c0cef
Tanish Manku <tanish2k09.dev@gmail.com>
2020-08-24 18:34:11 -0700

    LightButton: Make button bigger
    Signed-off-by: Tanish Manku <tanish2k09.dev@gmail.com>

--- src/components/LightButton.jsx
+++ src/components/LightButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function LightButton(props) {
     return (
         <a href={props.link}>
-            <button className="font-mono text-base tracking-widest rounded-lg p-4 m-8 min:min-w-full lg:min-w-25v light-button-bg flex justify-center">
+            <button className="font-mono text-base tracking-widest rounded-bigg p-8 m-8 min:min-w-full lg:min-w-25v light-button-bg flex justify-center">
                 <img src={props.asset} alt={"Vector logo of " + props.title} className={props.imgClasses + " inline-block mr-4"} />
                 {props.title.toString().toUpperCase()}
             </button>
