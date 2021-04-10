6bd16d8f017990cf384665b8d53d000b060daf96
Tanish Manku <tanish2k09.dev@gmail.com>
2021-04-09 20:08:07 -0700

    Revert "Make some changes for April Fools edition"
    This reverts commit 7411e5207221ea5cddd16ed73677ea4c104c3851.

--- src/components/RotatingSelfSegment.jsx
+++ src/components/RotatingSelfSegment.jsx
@@ -4,10 +4,14 @@ import TextLoop from "react-text-loop";
 function RotatingSelfSegment() {
   return (
     <TextLoop interval={1800} springConfig={{ stiffness: 300, damping: 50 }}>
-      <span>April fool's edition</span>
-      <span>April fool's edition</span>
-      <span>April fool's edition</span>
-      <span>pretty funny XD</span>
+      <span>am a Software Developer</span>
+      <span>care about accessibility</span>
+      <span>make some cool stuff</span>
+      <span>constantly expand my limits</span>
+      <span>really like minimalism</span>
+      <span>am a UI/UX Enthusiast</span>
+      <span>enjoy open source</span>
+      <span>love pancakes! Oh, and teal!</span>
     </TextLoop>
   );
 }
