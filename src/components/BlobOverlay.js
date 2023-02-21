001348e32fb922163b7a641fb371287393654118
Tanish Manku <tanish2k09.dev@gmail.com>
2023-02-21 12:33:41 -0800

    Blobby update (sort of) New blob revision features: - Superpositioned sin-wave based curve instead of bezier - Dynamic energy flux - Better phase shift (independent of FPS) - Slightly reworked mouse-energy interaction logic - Separate interaction logic into different file - Fixed a bug where blob cycled between REST and DECREASING states
    Signed-off-by: Tanish Manku <tanish2k09.dev@gmail.com>

--- src/components/BlobOverlay.js
+++ src/components/BlobOverlay.js
@@ -8,8 +8,12 @@ import { BlobInterpolator, interpolatorValue } from "./BlobInterpolator.js";
 const canvas = document.getElementById("vector_canvas");
 const ctx = canvas.getContext("2d");
 
-var gScale = 1;
+const HALF_PI = Math.PI / 2;
+const PI = Math.PI;
+const radians = deg => (deg * PI) / 180.0;
+const map = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
 
+var gScale = 1;
 if (window.devicePixelRatio < 2) {
   gScale = 2;
 }
@@ -29,72 +33,50 @@ const blobEnergyStates = {
 };
 
 const scaleDuration = 750; // milliseconds
+const energizeDuration = 1000; // milliseconds
 const fillColorDark = 'rgba(65, 255, 201, 255)';
 const fillColorDarkShadow = 'rgba(32, 128, 100, 25)';
 const fillColor = 'rgba(48, 227, 202, 255)';
 const fillColorShadow = 'rgba(48, 227, 202, 25)';
-const reactivePollInterval = 16.66;
-const widthBreakPoint = 768;
+export const POLL_INTERVAL = 200;
 
 let scaleInterpolator = new BlobInterpolator(scaleDuration);
-let energyInterpolator = new BlobInterpolator(scaleDuration);
-
-var anchorResizeToken = null;
+let energyInterpolator = new BlobInterpolator(energizeDuration);
 
 class Blob {
-  constructor(number, sectorAngle, minDeviation) {
+  constructor() {
     // use this to change the size of the screen estate to cover, in the minimum dimension
     this.screenEstateCoverageV = 0.6;
     this.screenEstateCoverageH = 0.6;
+    this.radiusOffset = 0;
 
-    if (minDeviation > Math.PI * 2) {
-      minDeviation = Math.PI * 2;
-    }
+    this.startingAngle = HALF_PI;
+    this.sectorAngle = HALF_PI;
 
-    // think of this as detail level
-    // number of conections in the `bezierSkin`
-    this.segments = number;
-    this.step = sectorAngle / this.segments;
-
-    // Stores points that are "pinned" and are points where bumps occur
-    this.anchors = [];
-
-    // Controls angular deviation
-    this.thetaOff = [];
+    // Controls extent of "distortions"
+    // Let's say, 5% of the blob itself is the wave
+    this.fluxRatio = 0.05;
+    this.phase = Math.random() * PI * 2;
+    this.waveOneHarmonic = 5 + Math.floor(Math.random() * 3);
+    this.waveTwoHarmonic = this.waveOneHarmonic + 2 + Math.floor(Math.random() * 12);
+    this.waveOnePhaseMultiplier = 1 + Math.floor(Math.random() * 5);
+    this.waveTwoPhaseMultiplier = this.waveOnePhaseMultiplier + 1 + Math.floor(Math.random() * 12);
+    this.phaseFluxMin = 0.005;
+    this.phaseFluxMax = 0.03;
+    this.phaseFlux = this.phaseFluxMin;
+    this.phaseEnergyOffset = 0;
+    this.phaseEnergyOffsetMaxDeviation = 16;
+    this.phaseEnergyOffsetMinDeviation = 1;
+    this.renewPhaseEnergyOffset();
+    console.log(`${this.waveOneHarmonic} - ${this.waveOnePhaseMultiplier}, ${this.waveTwoHarmonic} - ${this.waveTwoPhaseMultiplier}`)
 
     // Calculate radius
     this.updateValues();
-
-    // Controls radius of buldge
-    this.bumpRadius = this.baseRadius / 7;
-    this.halfBumpRadius = this.bumpRadius / 2;
-
-    // Just an added value to base radius
-    this.radiusOffset = 0;
-
-    // Keeps track of current bump radii
-    this.radii = [];
-
-    for (let i = 0; i < this.segments + 2; i++) {
-      this.anchors.push(0, 0);
-      this.radii.push(Math.random() * this.bumpRadius - this.halfBumpRadius);
-      this.thetaOff.push(Math.random() * (Math.PI * 2 - minDeviation) + minDeviation);
-    }
-
-    this.theta = 0;
-    this.thetaRamp = 0;
-    this.thetaRampDest = 12;
-    this.rampDamp = Math.pow(5, Math.max(10 - (window.devicePixelRatio / 0.25), 1));
-    this.thetaDelta = this.getBaseThetaDelta();
 
     // Track state
     this.state = blobStates.REGULAR;
     this.blobEnergyState = blobEnergyStates.REST;
     this.setDarkMode((localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)));
-  }
-
-  shouldRefresh() {
-    return this.isAnimating() || (this.thetaRamp < this.thetaRampDest * 0.99);
   }
 
   isAnimating() {
@@ -116,58 +98,46 @@ class Blob {
       return;
     }
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (this.shouldRefresh()) {
-      this.updateValues();
-    }
-
-    this.updateAnchors();
-
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-
-    bezierSkin(this.anchors, false);
-
-    ctx.lineTo(canvas.width, 0);
-    ctx.fillStyle = this.fillColor;
-    ctx.shadowBlur = 40 * gScale;
-    ctx.shadowColor = this.fillShadow;
-    ctx.fill();
+    this.updateValues();
+    this.draw();
   }
 
   updateValues() {
     canvas.width = window.innerWidth * gScale;
     canvas.height = window.innerHeight * gScale;
     this.baseRadius = this.getDiagonal() * 0.4;
+    this.flux = this.baseRadius * this.fluxRatio;
   }
 
-  getBaseThetaDelta() {
-    // For 1080p -> 1627 should be good at 0.0027
-    // For macbooks -> 2059 should be good at 0.016, 0.06
-    let density = window.devicePixelRatio / 0.25;
-
-    return Math.min(Math.pow(10, density - 10) * 2, 0.03);
+  updatePhase() {
+    this.phase += this.phaseFlux;
   }
 
-  getMaxThetaDelta() {
-    return Math.min(this.getBaseThetaDelta() * 8, 0.08);
-  }
+  draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  updateAnchors() {
-    this.thetaRamp += (this.thetaRampDest - this.thetaRamp) / this.rampDamp;
-    this.theta += this.thetaDelta;
+    // Move to the top right corner
+    ctx.beginPath();
+    ctx.moveTo(canvas.width, 0);
 
-    // Recreate anchors, initially with first anchor
-    this.anchors = [canvas.width, this.baseRadius];
+    // Draw the circular wave
+    let endAngle = this.startingAngle + this.sectorAngle;
+    for (let angle = this.startingAngle; angle < endAngle; angle += radians(0.05)) {
+      // The circular wave is a superposition of 2 sin waves
+      let r = Math.sin((angle - this.startingAngle) * this.waveOneHarmonic - this.phase * this.waveOnePhaseMultiplier) * this.flux / 2;
+      r += Math.sin((angle - this.startingAngle) * (this.waveTwoHarmonic + this.phaseEnergyOffset) + this.phase * this.waveTwoPhaseMultiplier) * this.flux / 2;
+      r += this.baseRadius + this.radiusOffset;
 
-    for (let i = 0; i <= this.segments + 2; i++) {
-      const sine = Math.sin(this.thetaOff[i] + this.theta + this.thetaRamp);
-      const rad = this.baseRadius + this.radiusOffset + this.radii[i] * sine;
-      const x = rad * Math.sin(this.step * i);
-      const y = rad * Math.cos(this.step * i);
-      this.anchors.push(canvas.width - x, y);
+      let x = r * Math.cos(angle);
+      let y = r * Math.sin(angle);
+      ctx.lineTo(canvas.width + x, y);
     }
+
+    ctx.closePath();
+    ctx.fillStyle = this.fillColor;
+    ctx.shadowBlur = 40 * gScale;
+    ctx.shadowColor = this.fillShadow;
+    ctx.fill();
   }
 
   getDiagonal() {
@@ -241,12 +211,8 @@ class Blob {
     return (this.getMaxRadius() - this.baseRadius) * multiplier;
   }
 
-  getMultiplierToThetaDelta(multiplier) {
-    return (this.getMaxThetaDelta() - this.getBaseThetaDelta()) * multiplier + this.getBaseThetaDelta();
-  }
-
   getMaxRadius() {
-    return this.getDiagonal() + this.bumpRadius;
+    return this.getDiagonal() + this.flux;
   }
 
   isMaximized() {
@@ -254,21 +220,24 @@ class Blob {
   }
 
   /* Converts given px acceleration to energy value */
-  reactivePx(speed) {
+  reactivePx(isAboveThreshold) {
+    // One-shot the increase if it was ever trigger, let it complete
     if (this.blobEnergyState === blobEnergyStates.INCREASING) {
       return;
     }
 
-    // Here we calculate the threshold for minimum movement required for energy gain
-    // Formula: (+ve Speed pixels / Diagonal pixels) * 100
-    // This is at max 1 / reactivePollInterval (0.06 at 60fps) and 0 at min
-    // We wanna normalize it to "0.1% screen in 1 interval" as threshold
-    let isAboveThreshold = Math.abs(speed) > (this.getDiagonal() * 0.001);
-
+    // Should increase energy
+    // If blob is already decreasing, it will be forced to increase again
     if (isAboveThreshold) {
       energyInterpolator.trackTime();
       this.blobEnergyState = blobEnergyStates.INCREASING;
-    } else if (this.blobEnergyState !== blobEnergyStates.DECREASING) {
+      return;
+    }
+
+    // Not enough interaction, should decay energy
+    // Blob is either maximum or at rest
+    // Don't bother blob if it's at REST.
+    if (this.blobEnergyState === blobEnergyStates.MAXIMUM) {
       energyInterpolator.trackTime();
       this.blobEnergyState = blobEnergyStates.DECREASING;
     }
@@ -289,19 +258,25 @@ class Blob {
     }
   }
 
+  renewPhaseEnergyOffset() {
+    this.phaseEnergyOffsetMax = Math.floor(map(Math.random(), 0, 1, this.phaseEnergyOffsetMinDeviation, this.phaseEnergyOffsetMaxDeviation)) * HALF_PI;
+  }
+
   reduceEnergy() {
     if (this.blobEnergyState === blobEnergyStates.REST) {
       return;
     }
 
-    if (this.thetaDelta === this.getBaseThetaDelta() || energyInterpolator.currentTimeFraction < 0) {
+    if (this.phaseEnergyOffset === 0 || energyInterpolator.currentTimeFraction < 0) {
+      // A fun little addition, we change the energetic phase shift each time it resets
+      this.renewPhaseEnergyOffset();
       this.blobEnergyState = blobEnergyStates.REST;
     }
 
     energyInterpolator.syncSubtractFraction();
-    this.thetaDelta = this.getMultiplierToThetaDelta(
-      interpolatorValue(energyInterpolator.currentTimeFraction)
-    );
+    let interpolationFraction = interpolatorValue(energyInterpolator.currentTimeFraction);
+    this.phaseEnergyOffset = interpolationFraction * this.phaseEnergyOffsetMax;
+    this.phaseFlux = interpolationFraction * (this.phaseFluxMax - this.phaseFluxMin) + this.phaseFluxMin;
   }
 
   increaseEnergy() {
@@ -313,20 +288,14 @@ class Blob {
       this.blobEnergyState = blobEnergyStates.MAXIMUM;
     }
 
-
     energyInterpolator.syncAddFraction();
-    this.thetaDelta = this.getMultiplierToThetaDelta(
-      interpolatorValue(energyInterpolator.currentTimeFraction)
-    );
+    let interpolationFraction = interpolatorValue(energyInterpolator.currentTimeFraction);
+    this.phaseEnergyOffset = interpolationFraction * this.phaseEnergyOffsetMax;
+    this.phaseFlux = interpolationFraction * (this.phaseFluxMax - this.phaseFluxMin) + this.phaseFluxMin;
   }
 }
 
-const blob = new Blob(10, Math.PI / 2, Math.PI / 2);
-
-function commitResize() {
-  blob.updateValues();
-  anchorResizeToken = null;
-}
+const blob = new Blob(Math.PI / 2, Math.PI / 2);
 
 function loop() {
   blob.energize();
@@ -338,91 +307,17 @@ function loop() {
 // Repeat the animation frames
 loop();
 
-// array of xy coords, closed boolean
-function bezierSkin(bez, closed = true) {
-  const avg = calcAvgs(bez);
-  const leng = bez.length;
-
-  if (closed) {
-    ctx.moveTo(avg[0], avg[1]);
-
-    for (let i = 2; i < leng; i += 2) {
-      let n = i + 1;
-      ctx.quadraticCurveTo(bez[i], bez[n], avg[i], avg[n]);
-    }
-
-    ctx.quadraticCurveTo(bez[0], bez[1], avg[0], avg[1]);
-  } else {
-    ctx.moveTo(bez[0], bez[1]);
-    ctx.lineTo(avg[0], avg[1]);
-
-    for (let i = 2; i < leng - 2; i += 2) {
-      let n = i + 1;
-      ctx.quadraticCurveTo(bez[i], bez[n], avg[i], avg[n]);
-    }
-
-    ctx.lineTo(bez[leng - 2], bez[leng - 1]);
-  }
+// I hate to move this outside the blob but phase should be FPS-locked:
+const PHASE_UPDATE_INTERVAL = 16; // Milliseconds
+var phaseStartTime = performance.now();
+function updateBlobPhase() {
+    blob.updatePhase();
+    setTimeout(updateBlobPhase, PHASE_UPDATE_INTERVAL);
 }
-
-// create anchor points by averaging the control points
-function calcAvgs(p) {
-  const avg = [];
-  const leng = p.length;
-  let prev;
-
-  for (let i = 2; i < leng; i++) {
-    prev = i - 2;
-    avg.push((p[prev] + p[i]) / 2);
-  }
-
-  // close
-  avg.push((p[0] + p[leng - 2]) / 2, (p[1] + p[leng - 1]) / 2);
-  return avg;
-}
+updateBlobPhase();
 
 export function getBlob() {
   return blob;
 }
 
-/* ----------- Attach Listeners ----------- */
-
-window.addEventListener("resize", function (event) {
-  if (window.innerWidth < widthBreakPoint) {
-    return;
-  }
-
-  if (anchorResizeToken != null) {
-    clearTimeout(anchorResizeToken);
-  }
-
-  anchorResizeToken = setTimeout(commitResize, 200);
-}, false);
-
-/* ----------- Attach Mouse Reaction ----------- */
-var lastEvent, currentEvent;
-
-document.onmousemove = function (event) {
-  currentEvent = event || window.event;
-}
-
-// Should be about 60fps
-setInterval(motionReactiveHook, reactivePollInterval);
-
-function motionReactiveHook() {
-  var speed = 0;
-
-  // If we have information for two intances to diff, proceed.
-  if (lastEvent && currentEvent) {
-    var movementX = Math.abs(currentEvent.screenX - lastEvent.screenX);
-    var movementY = Math.abs(currentEvent.screenY - lastEvent.screenY);
-    var movement = Math.hypot(movementX, movementY);
-
-    // Speed = total distance moved / time since last record
-    speed = movement / reactivePollInterval;
-
-    blob.reactivePx(speed);
-  }
-
-  lastEvent = currentEvent;
-}
+require('./BlobInteraction');
