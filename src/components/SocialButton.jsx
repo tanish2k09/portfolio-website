32c4268580139baa5f55639b30520a4fc9df8dd5
Tanish Manku <tanish2k09.dev@gmail.com>
2023-07-04 04:28:06 -0700

    Remove unneccesary concatenation

--- src/components/SocialButton.jsx
+++ src/components/SocialButton.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export default function SocialButton(props) {
     return (
         <a href={props.link}>
-            <button className="group social-button font-mono xs:text-xs md:text-base tracking-wide flex justify-center">
-                <props.asset className={"inline-block mr-2 ml-2 h-8 " + `${props.colors} ${props.activeLogoColors}`} />
+            <button className="group social-button font-mono text-xs md:text-base tracking-wide flex justify-center">
+                <props.asset className={`inline-block mr-2 ml-2 h-8 ${props.colors} ${props.activeLogoColors}`} />
                 {/* <img src={props.asset} alt={"Vector logo of " + props.title} className={props.imgClasses + " inline-block mr-6 h-8"} /> */}
-                <span className={"flex h-full items-center my-auto mr-6 md:group-hover:max-w-social " + `${props.textColors} ${props.activeColors}`}>{props.title.toString()}</span>
+                <span className={`flex h-full items-center my-auto mr-6 md:group-hover:max-w-social ${props.textColors} ${props.activeColors}`}>{props.title.toString()}</span>
             </button>
         </a>
     )
