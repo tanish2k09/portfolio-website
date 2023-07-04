87df2c1b59ab74d407e1e81e2a88bbeab46101ed
Tanish Manku <tanish2k09.dev@gmail.com>
2023-07-03 23:04:45 -0700

    Setup socials bar for color customization

--- src/components/ContactSocials.jsx
+++ src/components/ContactSocials.jsx
@@ -6,28 +6,57 @@ import {ReactComponent as LinkedInIcon} from '../assets/linkedin.svg';
 import {ReactComponent as TwitterIcon} from '../assets/twitter.svg';
 
 
-export default function ContactSocials() {
+export default function ContactSocials(props) {
+
+    var colors = "fill-contactlight dark:fill-contactdark"
+    var textColors = "text-contactlight dark:text-contactdark"
+    var activeColors = "text-dark"
+
+    if (props.colors) {
+        colors = props.colors;
+    }
+
+    if (props.textColors) {
+        textColors = props.textColors;
+    }
+
+    if (props.activeColors) {
+        activeColors = props.activeColors
+    }
+
     return (
         <div className="md:mb-4 md:flex">
             <SocialButton
                 title="Email - tmanku.dev@gmail.com"
                 link="mailto:tmanku.dev@gmail.com"
                 asset={EmailIcon}
+                colors={colors}
+                textColors={textColors}
+                activeColors={activeColors}
             />
             <SocialButton
                 title="LinkedIn - Tanish Manku"
                 link="https://www.linkedin.com/in/tmanku/"
                 asset={LinkedInIcon}
+                colors={colors}
+                textColors={textColors}
+                activeColors={activeColors}
             />
             <SocialButton
                 title="GitHub - tanish2k09"
                 link="https://www.github.com/tanish2k09/"
                 asset={GithubIcon}
+                colors={colors}
+                textColors={textColors}
+                activeColors={activeColors}
             />
             <SocialButton
                 title="Twitter - @tanish2k09"
                 link="https://www.twitter.com/tanish2k09"
                 asset={TwitterIcon}
+                colors={colors}
+                textColors={textColors}
+                activeColors={activeColors}
             />
         </div>
     )
--- src/components/SocialButton.jsx
+++ src/components/SocialButton.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 export default function SocialButton(props) {
 
+    let hoverColors = props.activeColors
+        .split(' ')
+        .map(color => 'group-hover:' + color)
+        .join(' ');
+
     return (
         <a href={props.link}>
-            <button className="social-button font-mono min:text-xs md:text-base tracking-wide flex justify-center">
-                <props.asset className="inline-block mr-2 ml-2 h-8 fill-contactlight dark:fill-contactdark"/>
+            <button className="group social-button font-mono min:text-xs md:text-base tracking-wide flex justify-center">
+                <props.asset className={"inline-block mr-2 ml-2 h-8 " + props.colors}/>
                 {/* <img src={props.asset} alt={"Vector logo of " + props.title} className={props.imgClasses + " inline-block mr-6 h-8"} /> */}
-                <span className="flex h-full items-center my-auto mr-6 text-contactlight dark:text-contactdark">{props.title.toString()}</span>
+                <span className={"flex h-full items-center my-auto mr-6 md:group-hover:max-w-social " + `${props.textColors} ${hoverColors}`}>{props.title.toString()}</span>
             </button>
         </a>
     )
