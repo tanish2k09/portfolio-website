0e5498221f8c71e9920816f00e85b6d0fb735912
Tanish Manku <tanish2k09.dev@gmail.com>
2023-07-07 09:52:11 -0700

    Implement logos overlay

--- src/scripts/AppThemeToggle.js
+++ src/scripts/AppThemeToggle.js
@@ -1,22 +1,24 @@
 import { getBlob } from "../components/BlobOverlay";
+import { toggleLogosDarkMode } from "../components/LogoOverlay";
 
 const html = document.getElementById("html_parent");
 const blob = getBlob();
 
 function lighten() {
-    blob.setDarkMode(false)
-    html.classList.remove("dark");
+  html.classList.remove("dark");
 }
 
 function darken() {
-    blob.setDarkMode(true);
-    html.classList.add("dark");
+  html.classList.add("dark");
 }
 
 export default function toggleAppTheme(shouldDarken) {
-    if (shouldDarken) {
-        darken()
-    } else {
-        lighten();
-    }
+  if (shouldDarken) {
+    darken();
+  } else {
+    lighten();
+  }
+
+  blob.setDarkMode(shouldDarken);
+  toggleLogosDarkMode(shouldDarken);
 }
