93029534ddedd63f49bbac0c5f39f8a37b0e28ff
Tanish Manku <tanish2k09.dev@gmail.com>
2023-11-02 17:57:39 -0700

    Remove Halloween-related TextLoop shorts

--- src/components/RotatingSelfSegment.jsx
+++ src/components/RotatingSelfSegment.jsx
@@ -5,11 +5,8 @@ function RotatingSelfSegment() {
   return (
     <TextLoop interval={1800} springConfig={{ stiffness: 300, damping: 50 }}>
       <span>am a Software Developer 💻</span>
-      <span>love Halloween! 🎃 👻</span>
       <span>make some cool stuff 🦄</span>
-      <span>am very scary. Boo! 👻</span>
-      <span>really like minimalism.</span>
-      <span>like pumpkin spice coffee ☕️</span>
+      <span>really like minimalism 🖋️</span>
       <span>am a UI/UX Enthusiast 🤳</span>
       <span>care about accessibility 🤝</span>
       <span>love teal! Oh, and pancakes! 🥞</span>
