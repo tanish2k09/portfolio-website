6a4d85d5123f9f990013af6180e8551b34d0eb24
Tanish Manku <tanish2k09.dev@gmail.com>
2024-01-19 00:40:50 -0800

    Write custom hooks and utils
    Signed-off-by: Tanish Manku <tanish2k09.dev@gmail.com>

---
+++ src/hooks/UseWorker.jsx
@@ -0,0 +1,15 @@
+import { useRef } from "react";
+import { Blob, HALF_PI } from "../components/Blobs/BlobOverlay.js";
+
+export const useWorker = () => {
+    const workerRef = useRef(null);
+
+    // Create new instance only if it doesn't exist
+    if (workerRef.current === null) {
+        workerRef.current = {
+            isBlob: true,
+            instance: new Blob(HALF_PI, HALF_PI),
+        }
+    }
+    return workerRef.current;
+}
