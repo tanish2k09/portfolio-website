645b5c9f3626b1d646a3c73311039ffe16782e90
Tanish Manku <tanish2k09.dev@gmail.com>
2024-01-25 17:36:10 -0800

    Add more padding for mobile Home CTA

--- src/components/Sections/BigName/AccentedButton.jsx
+++ src/components/Sections/BigName/AccentedButton.jsx
@@ -3,17 +3,17 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 
 export const common =
   "transition duration-500 ease-out transform hover:scale-110 " +
-  "font-body font-semibold tracking-wide " +
-  "flex px-2 py-4 sm:p-4 ";
+  "font-body font-semibold tracking-wide text-sm md:text-base " +
+  "flex py-4 p-4 ";
 
 export const primary =
   "bg-accent text-black group hover:bg-black dark:hover:bg-black hover:text-primary " +
-  "m-0 text-sm md:text-base " +
+  "m-0 " +
   "rounded border-accenttrim border-4";
 
 export const secondary =
   "bg-accentsecondary text-black group hover:bg-black dark:hover:bg-black hover:text-primary " +
-  "m-0 md:ml-6 mt-6 md:mt-auto text-sm md:text-base " +
+  "m-0 md:ml-6 mt-6 md:mt-auto " +
   "rounded-full border-accentsecondarytrim border-4";
 
 export default function AccentedButton(props) {
