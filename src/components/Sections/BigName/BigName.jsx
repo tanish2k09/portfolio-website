3da002b02df64a73c37bf5305114a2cdaa4d4a39
Tanish Manku <tanish2k09.dev@gmail.com>
2024-01-29 23:17:13 -0800

    Fix large layout spacing in BigName on mobile

--- src/components/Sections/BigName/BigName.jsx
+++ src/components/Sections/BigName/BigName.jsx
@@ -13,9 +13,9 @@ function BigName() {
           <br className="md:hidden" />
           Manku
         </div>
-        <div className="flex md:text-xs big-text-sec mt-2 items-center">
+        <div className="flex md:text-xs big-text-sec md:mt-2 items-center">
           <div className="text-textdark dark:text-secondary big-text-sec transition-all ">and I&nbsp;</div>
-          <div id="text_morph_container" className="big-text-sec text-textdarker dark:text-accenttrim dark:bg-accenttrim/10 dark:backdrop-blur-sm dark:px-2 py-3 rounded-lg transition-all">
+          <div id="text_morph_container" className="big-text-sec text-textdarker dark:text-accenttrim dark:bg-accenttrim/10 dark:backdrop-blur-sm dark:px-1.5 dark:md:px-2 py-1.5 md:py-3 rounded-lg transition-all">
             <RotatingSelfSegment />
           </div>
         </div>
--- src/components/Sections/BigName/NameBlock.jsx
+++ src/components/Sections/BigName/NameBlock.jsx
@@ -16,7 +16,7 @@ const NameBlock = memo(() => {
     return <section id="first_screen_block" className="h-screen relative flex items-center align-middle justify-center">
         <div id="corner_block" className="md:w-fit w-full rounded-xl mx-8 px-4 md:pl-8 md:pr-32 py-6 md:min-w-[50%] h-fit md:mr-48 bg-white/[.15] dark:bg-black/5 backdrop-blur-xl backdrop-contrast-[1.2] dark:backdrop-blur-md dark:backdrop-contrast-100 border-[2px] dark:border-[3px] border-white/30 dark:border-gray-800/20 transition-all duration-700">
             <BigName />
-            <div id="click_button_container" className="md:flex mt-16 lg:mt-20">
+            <div id="click_button_container" className="md:flex mt-8 md:mt-16 lg:mt-20">
                 <AccentedButton text="My work" link="#work_showcase_text" classes={primary} svg={cookie_svg} />
                 <AccentedButton text="Got a job? Let's talk!" link="#contact_me_text" classes={secondary} svg={plane_svg} />
             </div>
